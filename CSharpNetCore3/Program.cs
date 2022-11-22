734f882f75d9b12b9365883782a2f332f8d188f4
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-11-22 11:22:44 -0300

    Add project files.

---
+++ CSharpNetCore3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dynamic;
+
+namespace CSharpNetCore3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int valor = 10;
+            double valorDouble = 10.5;
+
+
+
+            Console.Read();
+        }
+    }
+}
