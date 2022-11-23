aba7647b37346f4bde6fdecb87d3d6745e058a6b
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-11-23 15:02:25 -0300

    Function increase payment

--- TasksDotNetCSharp/Program.cs
+++ TasksDotNetCSharp/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Tasks with .NET C#");
+﻿
+double payment, increase, newPayment; // Declarando as variaveis
+
+Console.WriteLine("Digite o salário do colaborador: ");
+payment = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o % de reajuste: ");
+increase = double.Parse(Console.ReadLine());
+increase = increase / 100;
+
+Console.WriteLine("Pressione qualquer tecla para visualizar o novo salário");
 Console.Read();
+// Console.Clear(); // limpar o console
+
+newPayment = payment + (payment * increase;
+
+Console.WriteLine($"Seu novo salário é de: {newPayment}");
+Console.Read();
+Console.WriteLine("Aperte alguma tecla para encerrar");
