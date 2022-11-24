6898ea5cf8e99f647a04bfd221ca8b2bc173ae5c
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-11-24 09:13:18 -0300

    fix: comentarios

--- TasksDotNetCSharp/Program.cs
+++ TasksDotNetCSharp/Program.cs
@@ -1,19 +1,22 @@
-﻿
-double payment, increase, newPayment; // Declarando as variaveis
+﻿// Cálculo de reajuste salarial
 
+// Declarando as variaveis
+double payment, increase, newPayment;
+
+// Capturando o salário do colaborador
 Console.WriteLine("Digite o salário do colaborador: ");
 payment = double.Parse(Console.ReadLine());
 
+// Capturando o % de reajuste
 Console.WriteLine("Digite o % de reajuste: ");
 increase = double.Parse(Console.ReadLine());
 increase = increase / 100;
-
 Console.WriteLine("Pressione qualquer tecla para visualizar o novo salário");
 Console.Read();
 // Console.Clear(); // limpar o console
 
+// Calculando e informando o novo salário
 newPayment = payment + (payment * increase);
-
 Console.WriteLine($"Seu novo salário é de: {newPayment}");
 Console.Read();
 Console.WriteLine("Aperte alguma tecla para encerrar");
