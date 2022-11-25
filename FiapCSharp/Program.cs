b7297579e0f27891e175f738ff1d151abadca83c
Jose Luiz Fagundes <jlfagundes@gmail.com>
2022-11-25 19:42:27 -0300

    Testando pressionar ESC pra sair

--- FiapCSharp/Program.cs
+++ FiapCSharp/Program.cs
@@ -5,6 +5,7 @@ public class Example
 {
     public static void Main()
     {
+        /*
         DateTime dat = DateTime.Now;
         Console.WriteLine("The time: {0:d} at {0:t}", dat);
         TimeZoneInfo tz = TimeZoneInfo.Local;
@@ -13,8 +14,15 @@ public class Example
                              tz.DaylightName : tz.StandardName);
         Console.Write("Press <Enter> to exit... ");
         while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        */
+        do
+        {
+            Console.WriteLine("A tecla pressionada não foi \"ESC");
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
     }
 }
 // The example displays output like the following:
 //     The time: 11/11/2015 at 4:02 PM:
 //     The time zone: Pacific Standard Time
+
+
